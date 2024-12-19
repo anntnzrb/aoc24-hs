b918f1fe38c9992d9ab9f5b3869299a1210a24a1
anntnzrb <anntnzrb@proton.me>
2024-12-19 08:36:59 -0500

    feat: add day 01 (p1)

--- src/Day01/Main.hs
+++ src/Day01/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import Data.List (sort, transpose)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import Text.Printf
+
+type LocationID = Int
+type LocationList = [LocationID]
+type LocationPair = (LocationList, LocationList)
+
+parseInput :: T.Text -> LocationPair
+parseInput s = case transpose parsed of
+    [xs, ys] -> (sort xs, sort ys)
+    _ -> error "Invalid. Expected exactly 2 lists."
+  where
+    notEmptyText = not . T.null
+    text2Int = read . T.unpack
+    parsed = map text2Int . filter notEmptyText . T.splitOn " " <$> T.lines s
+
+calcDistance :: LocationPair -> Int
+calcDistance (xs, ys) = sum $ zipWith diff xs ys
+  where
+    diff x y = abs $ x - y
+
+solve :: LocationPair -> Int
+solve = calcDistance
+
 main :: IO ()
 main = do
-    print "hi"
+    input <- parseInput <$> T.readFile "./inputs/day01.in"
+    printf "Part 1: %d\n" $ solve input
